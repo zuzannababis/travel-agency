d0429beae268d06b5477a8b698393bed232a02f2
Zuza Babis <zuzanna.babis@gmail.com>
2020-02-27 17:16:42 +0100

    Start OrderOptionDate

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -1,4 +1,32 @@
 /*import React from 'react';
 import DatePicker from 'react-datepicker';
 
-import 'react-datepicker/dist/react-datepicker-cssmodules.css';*/
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+
+class OrderOptionDate extends React.Component {
+  state = {
+    startDate: new Date()
+  };
+
+  handleChange = date => {
+    this.setState({
+      startDate: date
+    });
+  };
+
+  render() {
+    return (
+      <DatePicker
+        selected={this.state.startDate}
+        onChange={this.handleChange}
+      />
+    );
+  }
+}
+
+OrderOptionDate.propTypes = {
+  date:
+
+}
+
+export default OrderOptionDate; */
