ba4bc6d999e3b3d962453a37c00e211db9137254
Zuza Babis <zuzanna.babis@gmail.com>
2020-03-08 12:26:21 +0100

    Start working on promoPrice function

---
+++ src/utils/promoPrice.js
@@ -0,0 +1,3 @@
+/*export const promoPrice = (amount, discount) => {
+
+};*/
--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,4 +1,5 @@
 import { formatTime } from './formatTime';
+//import { promoPrice } from './promoPrice';
 
 describe('utils', () => {
   describe('formatTime', () => {
@@ -19,6 +20,12 @@ describe('utils', () => {
       expect(formatTime(3793)).toBe('01:03:13');
       expect(formatTime(120)).toBe('00:02:00');
       expect(formatTime(3604)).toBe('01:00:04');
+    });
+  });
+  describe('promoPrice', () => {
+
+    it('should calculate 20% smaller price', () => {
+      expect(formatTime()).toBe(null);
     });
   });
 });
